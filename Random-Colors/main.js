fb15b24ab77d070eec4f2134c431d71b95608e3e
Askar Pulatov <master15994@gmail.com>
2023-03-10 16:55:18 +0300

    Random - Colors JS

---
+++ Random-Colors/main.js
@@ -0,0 +1,61 @@
+const col = document.querySelectorAll('.col');
+
+document.addEventListener('keydown', event => {
+  event.preventDefault();
+  if (event.code.toLowerCase() === 'space') {
+    setRandomColors();
+  }
+});
+
+document.addEventListener('click', event => {
+  const type = event.target.dataset.type;
+
+  if (type === 'lock') {
+    const node =
+      event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0];
+    node.classList.toggle('fa-lock-open');
+    node.classList.toggle('fa-lock');
+  } else if (type === 'copy') {
+    copyToClickboard(event.target.textContent);
+  }
+});
+function generateRandomColors() {
+  const hexCodes = '0123456789ABCDEF';
+  let color = '';
+  for (i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return '#' + color;
+}
+
+function copyToClickboard(text) {
+  return navigator.clipboard.writeText(text);
+}
+
+function setRandomColors() {
+  col.forEach(el => {
+    const isLocked = el.querySelector('i').classList.contains('fa-lock');
+    const text = el.querySelector('h2');
+    const button = el.querySelector('button');
+    const color = chroma.random();
+
+    if (isLocked) {
+      return;
+    }
+
+    text.textContent = color;
+    el.style.background = color;
+
+    setTextColor(text, color);
+    setTextColor(button, color);
+  });
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+setRandomColors();
